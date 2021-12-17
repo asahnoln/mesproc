c005b87aac298af006ecdd0eb8444aac8a4e522c
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-12-17 07:50:37 +0600

    Working through http server
    - [REMOVED] All old code
    - [ADDED] Working through HTTP

--- processor.go
+++ processor.go
@@ -1,9 +1,36 @@
 package mesproc
 
-type Handler interface {
-	Handle()
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+)
+
+const (
+	OK_ANSWER = "ok"
+)
+
+type AnswerMap map[string]string
+
+type Server struct {
+	m      AnswerMap
+	target string
 }
 
-func HandleRequests(h Handler) {
-	h.Handle()
+func NewServer(m AnswerMap, target string) *Server {
+	return &Server{
+		m:      m,
+		target: target,
+	}
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var m struct {
+		Message string
+	}
+	_ = json.NewDecoder(r.Body).Decode(&m)
+
+	http.Post(s.target, "", strings.NewReader(`{"message": "`+s.m[m.Message]+`"}`))
+
+	w.Write([]byte(OK_ANSWER))
 }
--- processor_test.go
+++ processor_test.go
@@ -1,29 +1,49 @@
 package mesproc_test
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/asahnoln/mesproc"
 )
 
 func TestReceive(t *testing.T) {
-	service := &stubService{}
-	mesproc.HandleRequests(service)
+	srv := mesproc.NewServer(nil, "")
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(`{"message": "/start"}`))
+	srv.ServeHTTP(w, r)
 
-	if !service.handled {
-		t.Error("service was not handled by HandleRequests")
+	want := "ok"
+	got := w.Body.String()
+
+	if want != got {
+		t.Errorf("want response answer %q, got %q", want, got)
 	}
 }
 
-type stubService struct {
-	handled bool
-}
+func TestSend(t *testing.T) {
+	m := mesproc.AnswerMap{
+		"/start": "Choose your language",
+	}
+	var got string
+	service := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var m struct {
+			Message string
+		}
+		json.NewDecoder(r.Body).Decode(&m)
+		got = m.Message
+	}))
 
-func (s *stubService) Handle() {
-	s.handled = true
-}
+	srv := mesproc.NewServer(m, service.URL)
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(`{"message": "/start"}`))
+	srv.ServeHTTP(w, r)
 
-func TestHttpHandlerHandled(t *testing.T) {
-	h := mesproc.NewHttpHandler()
-	mesproc.HandleRequests(h)
+	want := m["/start"]
+	if want != got {
+		t.Errorf("want service receive message %q, got %q", want, got)
+	}
 }
