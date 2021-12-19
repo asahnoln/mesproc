377f47d2bfdd529c55b750b1e819bbaef997195d
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-12-20 05:35:34 +0600

    Main cmd
    - [ADDED] Main file to test server with handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/asahnoln/mesproc"
+)
+
 func main() {
+	tg := mesproc.NewTgHandler(os.Getenv("BOT_ADDR"))
+
+	srv := mesproc.NewServer(tg)
+
+	log.Fatalln(http.ListenAndServe(os.Getenv("SRV_ADDR"), srv))
 }
