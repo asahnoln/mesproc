ab55de10ad5fb0f96129a3e6bb7af84ea505ef87
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-12-19 16:32:13 +0600

    Story test is more about the story
    - [CHANGED] Command to simple statements
    - [CHANGED] Refactor - use given map in assertions
    - [CHANGED] Updated TODO

--- story_test.go
+++ story_test.go
@@ -7,15 +7,19 @@ import (
 )
 
 func TestRespond(t *testing.T) {
+	const (
+		s1 = "sector 1"
+		s2 = "sector 2"
+	)
 	m := mesproc.AnswerMap{
-		"/start": "Choose language",
-		"/ru":    "Choose sector",
+		s1: "That's the story of sector 1. Now type `sector 2`",
+		s2: "Final story of sector 2",
 	}
 	s := mesproc.NewStory(m)
 
-	got := s.Respond("/start")
-	assertSameString(t, "Choose language", got, "want response %q, got %q")
+	got := s.Respond(s1)
+	assertSameString(t, m[s1], got, "want response %q, got %q")
 
-	got = s.Respond("/ru")
-	assertSameString(t, "Choose sector", got, "want response %q, got %q")
+	got = s.Respond(s2)
+	assertSameString(t, m[s2], got, "want response %q, got %q")
 }
