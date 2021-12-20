3dc44dec1974a6343f00e955a3cd78db00e156eb
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-12-20 12:56:10 +0600

    Added bot path to route
    - [ADDED] Bot path to server handling

--- cmd/main.go
+++ cmd/main.go
@@ -10,8 +10,10 @@ import (
 
 func main() {
 	tg := mesproc.NewTgHandler(os.Getenv("BOT_ADDR"))
-
 	srv := mesproc.NewServer(tg)
 
-	log.Fatalln(http.ListenAndServe(os.Getenv("SRV_ADDR"), srv))
+	log.Fatalln(http.ListenAndServe(os.Getenv("SRV_PORT"), http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mux := http.NewServeMux()
+		mux.Handle(os.Getenv("SRV_BOT_PATH"), srv)
+	})))
 }
