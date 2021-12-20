de358c40bd4e06558a83c879768c02eedfe9357b
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2021-12-20 13:56:26 +0600

    Https server in main
    - [CHANGED] https server

--- cmd/main.go
+++ cmd/main.go
@@ -12,9 +12,11 @@ func main() {
 	tg := mesproc.NewTgHandler(os.Getenv("BOT_ADDR"))
 	srv := mesproc.NewServer(tg)
 
-	log.Fatalln(http.ListenAndServe(os.Getenv("SRV_PORT"), http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		mux := http.NewServeMux()
-		mux.Handle(os.Getenv("SRV_BOT_PATH"), srv)
-		mux.ServeHTTP(w, r)
-	})))
+	log.Fatalln(http.ListenAndServeTLS(
+		os.Getenv("SRV_PORT"), os.Getenv("CERT_FILE"), os.Getenv("KEY_FILE"),
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			mux := http.NewServeMux()
+			mux.Handle(os.Getenv("SRV_BOT_PATH"), srv)
+			mux.ServeHTTP(w, r)
+		})))
 }
