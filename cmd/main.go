82d36c1189bfe1284ac4908def173443cd9bc9b8
Arthur Aslanyan <arthur.e.aslanyan@gmail.com>
2022-01-03 14:24:57 +0600

    Updated test tg in main
    - [ADDED] i18n to story
    - [ADDED] Audio to story
    - [CHANGED] Moved story creation to function

--- cmd/main.go
+++ cmd/main.go
@@ -8,22 +8,39 @@ import (
 	"github.com/asahnoln/mesproc"
 )
 
+func story() *mesproc.Story {
+	return mesproc.NewStory().
+		Add(mesproc.NewStep().
+			Expect("sector 1").
+			Respond("move on to next sector").
+			Fail("Enter `sector 1`")).
+		Add(mesproc.NewStep().
+			Expect("sector 2").
+			Respond("audio:http://asabalar.kz/kazakhstan.mp3").
+			Fail("Enter `sector 2`")).
+		Add(mesproc.NewStep().
+			Expect("lulz").
+			Respond("finish here").
+			Fail("Enter `lulz`")).
+		I18n(mesproc.I18nMap{
+			"ru": {
+				"sector 1":               "сектор 1",
+				"sector 2":               "сектор 2",
+				"move on to next sector": "идите в следующий сектор",
+				"Enter `sector 2`":       "Введите `сектор 2`",
+				"Enter `lulz`":           "Введите `lulz`",
+			},
+			"kk": {
+				"sector 1":               "кз сектор 1",
+				"sector 2":               "кз сектор 2",
+				"move on to next sector": "кз идите в следующий сектор",
+				"Enter `sector 2`":       "кз Введите `сектор 2`",
+			},
+		})
+}
+
 func main() {
-	tg := mesproc.NewTgHandler(os.Getenv("BOT_ADDR"),
-		mesproc.NewStory().
-			Add(mesproc.NewStep().
-				Expect("sector 1").
-				Respond("move on to next sector").
-				Fail("Enter `sector 1`")).
-			Add(mesproc.NewStep().
-				Expect("sector 2").
-				Respond("go to next sector, yes, which is named 'lulz'").
-				Fail("Enter `sector 2`")).
-			Add(mesproc.NewStep().
-				Expect("lulz").
-				Respond("finish here").
-				Fail("Enter `lulz`")),
-	)
+	tg := mesproc.NewTgHandler(os.Getenv("BOT_ADDR"), story())
 	srv := mesproc.NewServer(tg)
 
 	log.Fatalln(http.ListenAndServeTLS(
